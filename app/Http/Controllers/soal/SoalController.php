27e8e01e2bbdbda408b2aa93fa830c145523eb47
rexywjy <rexywjy@gmail.com>
2024-06-06 09:16:43 +0700

    Update SoalController.php

--- app/Http/Controllers/soal/SoalController.php
+++ app/Http/Controllers/soal/SoalController.php
@@ -15,6 +15,7 @@ class SoalController extends Controller
      */
     public function index()
     {
+        // ini cek ada filter mapel nda
         if(request() -> has('mapel')){
             $mapel = request()->mapel;
             if($mapel == '0'){
@@ -26,7 +27,7 @@ class SoalController extends Controller
             return response()->json($soals);
         }
 
-        // ini gaada filter
+        // ini nda ada filter
         $mapels = DB::table('mata_pelajarans')->get();
         $soals = Pertanyaan::get();
         return view('soal.index', [
@@ -38,7 +39,6 @@ class SoalController extends Controller
     public function store(Request $request)
     {
         //
-        // dd($request->all());
         $request->validate([
             'id' => 'required',
             'jawaban' => 'required|mimes:pdf|max:10000'
@@ -49,12 +49,10 @@ class SoalController extends Controller
         $extension = $file->getClientOriginalExtension();
         $fileNameToStore = $originalName . '.' . $extension;
         $filePath = $file->storeAs('JawabanFiles', $fileNameToStore, 'public');
-        // $request->jawaban = $filePath;
 
         $updatedjawaban = Pertanyaan::find($request->id);
         $updatedjawaban->jawaban = $filePath;
         $updatedjawaban->save();
-        // return response()->json(['success' => true, 'message' => 'Data has been saved', 'data' => $updatedjawaban]);
         return redirect()->route('soal.index')->with( ['message' => 'Data has been saved!'] );
     }
 
@@ -63,15 +61,7 @@ class SoalController extends Controller
      */
     public function show(string $id)
     {
-        // dd($id, request()->all());
         //
-        // dd($id,request()->all());
-        // $soals = Soal::
-        // where('user_id', request()->user->id)
-        // ->get();
-        // return view('soal.index', [
-        //     'soals' => $soals
-        // ]);
     }
 
     /**
@@ -80,7 +70,6 @@ class SoalController extends Controller
     public function edit(string $id)
     {
         //
-        // dd($id, request()->all());
     }
     public function edit2(string $id)
     {
@@ -89,7 +78,6 @@ class SoalController extends Controller
             'pertanyaan' => $soalnya,
             'id' => $id
         ]);
-        // return view('soal.uploadjawaban');
     }
 
     /**
